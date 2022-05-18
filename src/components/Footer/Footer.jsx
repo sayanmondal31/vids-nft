1f5fcbe874df9c0047f9ff00f54e5694452014f1
sayanmondal31 <sayanm816@gmail.com>
2022-05-18 06:44:37 +0530

    footer update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -149,11 +149,7 @@ const Footer = () => {
           </Col>
 
           <Col lg="12" className=" mt-4 text-center">
-            <p className="copyright">
-              {" "}
-              Copyrights 2022, Developed by Muhibur Rahman. @Muhib's Tech Diary
-              All Rights Reserved.{" "}
-            </p>
+            <p className="copyright"> Copyrights 2022, All Rights Reserved. </p>
           </Col>
         </Row>
       </Container>
